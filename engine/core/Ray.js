148143cff2285783fef3375e9a955f9afbf8f757
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-11-13 08:41:22 +0700

    Update Ray.js

--- engine/core/Ray.js
+++ engine/core/Ray.js
@@ -1,14 +1,30 @@
-/**
- * Ray
- * --------------------------------------------------------------------
- * Role:
- * - Represents a ray with origin and direction for picking/collision.
- */
 import { Vector3 } from './Vector3.js';
 
-export class Ray {
-  constructor(origin = new Vector3(), direction = new Vector3(0,0,-1)) {
-    this.origin = origin.clone();
-    this.direction = direction.clone().normalize();
+/**
+ * Ray class
+ * Represents a ray with origin and direction.
+ * Used in raycasting and physics queries.
+ */
+class Ray {
+  constructor(origin = new Vector3(), direction = new Vector3(0, 0, -1)) {
+    this.origin = origin;
+    this.direction = direction;
+
+    this.isValid = true;
+    this.debugMode = false;
+  }
+
+  /**
+   * Gets a point along the ray at distance t.
+   */
+  at(t) {
+    return new Vector3(
+      this.origin.x + this.direction.x * t,
+      this.origin.y + this.direction.y * t,
+      this.origin.z + this.direction.z * t
+    );
   }
 }
+
+export { Ra
+        y };
