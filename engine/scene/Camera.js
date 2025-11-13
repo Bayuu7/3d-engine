d5b372dfcb758e09ae6f770ae53e10edc78d412b
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-11-13 09:01:45 +0700

    Update Camera.js

--- engine/scene/Camera.js
+++ engine/scene/Camera.js
@@ -1,54 +1,101 @@
-import { Matrix4 } from '../core/Matrix4.js';
+import { Entity } from './Entity.js';
 import { Vector3 } from '../core/Vector3.js';
+import { Matrix4 } from '../core/Matrix4.js';
 
-export class Camera {
-  constructor(){
-    this.transform = { position: new Vector3(0,0,5) };
-    this.target = new Vector3(0,0,0);
-    this.up = new Vector3(0,1,0);
-    this.fov = 60 * Math.PI/180;
-    this.aspect = 16/9;
+/**
+ * Camera class
+ * A specialized Entity that provides view and projection matrices.
+ * Integrates with renderer to set the viewpoint for scene rendering.
+ */
+class Camera extends Entity {
+  constructor(name = 'Camera') {
+    // Call base Entity constructor
+    super(name);
+    // Field of view in degrees (vertical)
+    this.fov = 60;
+    // Near and far clipping planes for projection
     this.near = 0.1;
-    this.far = 100;
-    this.view = new Matrix4();
-    this.projection = new Matrix4();
-    this.orbitEnabled = false;
-    this.orbitRadius = 10;
-    this.orbitTheta = 0;
-    this.orbitPhi = Math.PI/4;
+    this.far = 1000;
+    // Aspect ratio width/height; can be set by renderer/viewport
+    this.aspect = 16 / 9;
+    // Cached view matrix (from inverse of world transform)
+    this.viewMatrix = new Matrix4();
+    // Cached projection matrix derived from fov/aspect/near/far
+    this.projectionMatrix = new Matrix4();
+    // Boolean flag: recompute matrices when transform or params change
+    this.dirty = true;
+    // Debug flag
+    this.debugMode = false;
   }
 
-  updateMatrices(){
-    // Projection
-    const f = 1/Math.tan(this.fov/2);
-    const nf = 1/(this.near - this.far);
-    const e = this.projection.elements;
-    e[0]=f/this.aspect; e[1]=0; e[2]=0; e[3]=0;
-    e[4]=0; e[5]=f; e[6]=0; e[7]=0;
-    e[8]=0; e[9]=0; e[10]=(this.far+this.near)*nf; e[11]=-1;
-    e[12]=0; e[13]=0; e[14]=2*this.far*this.near*nf; e[15]=0;
+  /**
+   * Marks camera matrices as dirty to recompute next frame.
+   */
+  markDirty() {
+    // Set dirty flag so matrices are recomputed
+    this.dirty = true;
+    // Optionally log change
+    if (this.debugMode) {
+      console.log(`[Camera:${this.name}] Marked dirty`);
+    }
+  }
 
-    // View (lookAt)
-    const eye=this.transform.position, center=this.target, up=this.up;
-    const zx=eye.x-center.x, zy=eye.y-center.y, zz=eye.z-center.z;
-    let rl=Math.hypot(zx,zy,zz); const zxN=zx/rl, zyN=zy/rl, zzN=zz/rl;
-    const xx=up.y*zzN - up.z*zyN, xy=up.z*zxN - up.x*zzN, xz=up.x*zyN - up.y*zxN;
-    rl=Math.hypot(xx,xy,xz); const xN=[xx/rl,xy/rl,xz/rl];
-    const yN=[zyN*xN[2]-zzN*xN[1], zzN*xN[0]-zxN*xN[2], zxN*xN[1]-zyN*xN[0]];
-    const eV=this.view.elements;
-    eV[0]=xN[0]; eV[1]=yN[0]; eV[2]=zxN; eV[3]=0;
-    eV[4]=xN[1]; eV[5]=yN[1]; eV[6]=zyN; eV[7]=0;
-    eV[8]=xN[2]; eV[9]=yN[2]; eV[10]=zzN; eV[11]=0;
-    eV[12]=-(xN[0]*eye.x+xN[1]*eye.y+xN[2]*eye.z);
-    eV[13]=-(yN[0]*eye.x+yN[1]*eye.y+yN[2]*eye.z);
-    eV[14]=-(zxN*eye.x+zyN*eye.y+zzN*eye.z);
-    eV[15]=1;
+  /**
+   * Sets camera parameters (fov/near/far/aspect) and marks dirty.
+   */
+  setParams({ fov = this.fov, near = this.near, far = this.far, aspect = this.aspect } = {}) {
+    // Update projection parameters
+    this.fov = fov;
+    this.near = near;
+    this.far = far;
+    this.aspect = aspect;
+    // Mark matrices to be recomputed
+    this.markDirty();
+  }
 
-    // Orbit update if enabled
-    if (this.orbitEnabled){
-      this.transform.position.x = this.target.x + this.orbitRadius*Math.sin(this.orbitPhi)*Math.cos(this.orbitTheta);
-      this.transform.position.y = this.target.y + this.orbitRadius*Math.cos(this.orbitPhi);
-      this.transform.position.z = this.target.z + this.orbitRadius*Math.sin(this.orbitPhi)*Math.sin(this.orbitTheta);
+  /**
+   * Recomputes view and projection matrices if dirty.
+   * Note: For simplicity, we assume renderer or math utils will provide inversion and perspective.
+   */
+  computeMatrices() {
+    // If nothing changed, skip recomputation
+    if (!this.dirty) return;
+
+    // Compute projection matrix (placeholder: you may replace with a proper perspective function)
+    const f = 1.0 / Math.tan((this.fov * Math.PI / 180) / 2);
+    const e = this.projectionMatrix.elements;
+    e[0] = f / this.aspect; e[1] = 0; e[2] = 0; e[3] = 0;
+    e[4] = 0; e[5] = f; e[6] = 0; e[7] = 0;
+    e[8] = 0; e[9] = 0; e[10] = (this.far + this.near) / (this.near - this.far); e[11] = -1;
+    e[12] = 0; e[13] = 0; e[14] = (2 * this.far * this.near) / (this.near - this.far); e[15] = 0;
+
+    // View matrix: for brevity, we reuse world matrix and expect renderer to invert it
+    const world = this.transform.getMatrix();
+    this.viewMatrix = world; // Replace with an actual inverse in your math lib
+
+    // Reset dirty flag after recomputation
+    this.dirty = false;
+
+    // Optionally log recomputation
+    if (this.debugMode) {
+      console.log(`[Camera:${this.name}] Matrices computed`);
+    }
+  }
+
+  /**
+   * Updates camera state per frame.
+   * @param {number} delta - Time delta in seconds.
+   */
+  onUpdate(delta) {
+    // Recompute matrices if needed
+    this.computeMatrices();
+    // Call base entity update to update components/children
+    super.update(delta);
+    // Optionally log update
+    if (this.debugMode) {
+      console.log(`[Camera:${this.name}] Updated with delta:`, delta);
     }
   }
 }
+
+export { Camera };
